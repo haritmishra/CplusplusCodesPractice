2af7d06541b248b810dac33d55598eb3c117ac67
Harit Mishra <hmishra_be20@thapar.edu>
2022-02-24 22:30:24 +0530

    first commit

---
+++ 2datatypes.cpp
@@ -0,0 +1,19 @@
+//Data Types
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "size of int " << sizeof(a) << endl;
+    float b;
+    cout << "size of float " << sizeof(b) << endl;
+    char c;
+    cout << "size of char " << sizeof(c) << endl;
+    bool d;
+    cout << "size of bool " << sizeof(d) << endl;
+    long int e;
+    cout << "size of long int " << sizeof(e) << endl;
+    short int f;
+    cout << "size of short int " << sizeof(f) << endl;
+}
---
+++ 3input2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sam;
+    cin >> sam;
+    int sammy;
+    cin >> sammy;
+    int sum = sam + sammy;
+    cout << "sum\n";
+    cout << sum << endl;
+    return 0;
+}
